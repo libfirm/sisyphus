d477a957d8662a521036148c5e1611039bfa1155
Matthias Braun <matze@braunis.de>
2013-02-05 23:39:19 +0100

    cleanup shell.py, remove some unused functions

--- util/shell.py
+++ util/shell.py
@@ -17,31 +17,33 @@ class SigKill(Exception):
         self.name = name
 
 
+def lower_rlimit(res, limit):
+    (soft, hard) = resource.getrlimit(res)
+    if soft > limit or soft == resource.RLIM_INFINITY:
+        soft = limit
+    if hard > limit or hard == resource.RLIM_INFINITY:
+        hard = limit
+    resource.setrlimit(res, (soft, hard))
+
+
+def set_rlimit(timeout):
+    if timeout > 0.0:
+        lower_rlimit(resource.RLIMIT_CPU, timeout)
+    MB = 1024 * 1024
+    lower_rlimit(resource.RLIMIT_CORE,  0)
+    lower_rlimit(resource.RLIMIT_DATA,  1024 * MB)
+    lower_rlimit(resource.RLIMIT_STACK, 1024 * MB)
+    lower_rlimit(resource.RLIMIT_FSIZE,   32 * MB)
+
+
 def execute(cmd, env=None, timeout=0):
     """Execute a command and return stderr and stdout data"""
-
-    def lower_rlimit(res, limit):
-        (soft, hard) = resource.getrlimit(res)
-        if soft > limit or soft == resource.RLIM_INFINITY:
-            soft = limit
-        if hard > limit or hard == resource.RLIM_INFINITY:
-            hard = limit
-        resource.setrlimit(res, (soft, hard))
-
-    def set_rlimit():
-        if timeout > 0.0:
-            lower_rlimit(resource.RLIMIT_CPU, timeout)
-        MB = 1024 * 1024
-        lower_rlimit(resource.RLIMIT_CORE,  0)
-        lower_rlimit(resource.RLIMIT_DATA,  1024 * MB)
-        lower_rlimit(resource.RLIMIT_STACK, 1024 * MB)
-        lower_rlimit(resource.RLIMIT_FSIZE,   32 * MB)
 
     cmd = filter(lambda x: x, cmd.split(' '))
     proc = subprocess.Popen(cmd,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
-                            preexec_fn=set_rlimit,
+                            preexec_fn=lambda: set_rlimit(timeout),
                             env=env)
     out, err = proc.communicate()
     returncode = proc.returncode
@@ -54,27 +56,6 @@ def execute(cmd, env=None, timeout=0):
     if returncode in _EXIT_CODES:
         raise SigKill(returncode, _EXIT_CODES[returncode])
     return (out, err, returncode)
-
-
-def silent_shell(cmd, env=None, debug=False):
-    """Execute a shell command"""
-    if debug:
-        sys.stderr.write("silent_shell: %s\n" % cmd)
-        stdout = None
-        stderr = None
-    else:
-        stdout = open("/dev/null", 'a')
-        stderr = subprocess.STDOUT
-    try:
-        return subprocess.call(cmd, shell=True, stdout=stdout, stderr=stderr, env=env)
-    except OSError as e:
-        sys.stderr.write("Execution failed: %s\n" % e)
-
-
-def write_file(filename, content):
-    fh = open(filename, 'w')
-    fh.write(content)
-    fh.close()
 
 
 if __name__ == "__main__":

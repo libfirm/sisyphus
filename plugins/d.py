b9cb0d8f69b3f35dd4cdf26665994ec15eb9f10d
Andreas Zwinkau <zwinkau@kit.edu>
2013-02-15 14:52:06 +0100

    Add plugin for D

---
+++ plugins/d.py
@@ -0,0 +1,20 @@
+import os
+from test.steps  import execute
+
+def step_compile_d(environment):
+	"""Compile file with DC without linking"""
+	cmd = "%(dc)s -c %(filename)s %(dflags)s -of%(executable)s" % environment.__dict__
+	return execute(environment, cmd, timeout=240)
+
+def step_compile_and_link_d(environment):
+	"""Compile file with DC to executable"""
+	cmd = "%(dc)s %(filename)s %(dflags)s -of%(executable)s" % environment.__dict__
+	return execute(environment, cmd, timeout=240)
+
+def register_arguments(argparser):
+	group = argparser.add_argument_group("D language")
+	group.add_argument("--dc", dest="dc", default="dmd",
+	                help="Use DC to compile D programs", metavar="DC")
+	group.add_argument("--dflags", dest="dflags", default="",
+	                help="Use flags to compile D programs")
+

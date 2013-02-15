fe59130efe56ae41e81575ff153760f7d0d21b6d
Andreas Zwinkau <zwinkau@kit.edu>
2013-02-15 14:38:20 +0100

    Some helper functions to avoid test.suite.make

--- test/suite.py
+++ test/suite.py
@@ -13,10 +13,18 @@ class _TestSuite(object):
 
 
 def get_default_suite():
+    """Returns the default test suite"""
     return _default_suite
 
+def add(test):
+    """Add a test case to the default test suite"""
+    global _default_suite
+    if _default_suite is None:
+        _default_suite = _TestSuite("default", [], None, None)
+    _default_suite.tests.append(test)
 
 def make(name, tests, environment=None, register_arguments=None):
+    """Create a test suite. Make it the default one, if there is no default so far."""
     global _default_suite
     if environment is None:
         environment = Environment()
@@ -27,3 +35,11 @@ def make(name, tests, environment=None, register_arguments=None):
     if _default_suite is None:
         _default_suite = suite
     return suite
+
+def set_environment(environment):
+    """Set the initial environment of the default test suite"""
+    _default_suite.environment = environment
+
+def set_register_arguments(register_arguments):
+    """Set the initial register_arguments function of the default test suite"""
+    _default_suite.register_arguments = register_arguments

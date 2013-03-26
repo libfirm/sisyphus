c48f5bdeea604f1a57745f9ec1b877a862347340
Matthias Braun <matthias.braun@kit.edu>
2013-03-26 17:44:19 +0100

    remove configs directory

--- configs/amd64.py
+++
@@ -1,9 +0,0 @@
-def config_amd64(argparser, namespace, values, option_string):
-    namespace.arch_dirs    = []
-    namespace.arch_cflags  = "-m64 -bisa=amd64"
-    namespace.arch_ldflags = ""
-    namespace.expect_url   = "fail_expectations_amd64"
-
-configurations = {
-    'amd64': config_amd64
-}

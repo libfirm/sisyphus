3faff5713d6d71b6c0fdb3aab66a61624e9960a8
Andreas Zwinkau <zwinkau@kit.edu>
2015-07-20 13:01:49 +0200

    more debug output

--- sisyphus/util/shell.py
+++ sisyphus/util/shell.py
@@ -75,10 +75,12 @@ class _Execute(object):
             if self.exception is not None:
                 raise self.exception
             if thread.is_alive():
+                _LOG.debug("timeout %.1f reached, terminate process" % self.timeout)
                 if self.proc is not None:
                     self.proc.terminate()
                     thread.join(1.0)
                     if thread.is_alive():
+                        _LOG.debug("termination ignored, now kill process")
                         self.proc.kill()
                         thread.join()
                 raise SigKill(signal.SIGXCPU, "SIGXCPU")
@@ -92,6 +94,7 @@ class _Execute(object):
         if self.returncode > 127:
             self.returncode = 128 - self.returncode
         if self.returncode in _EXIT_CODES:
+            _LOG.debug("returncode %d recognized as '%s'" % (self.returncode, _EXIT_CODES[self.returncode])
             raise SigKill(self.returncode, _EXIT_CODES[self.returncode])
         return (self.out, self.err, self.returncode)
 
